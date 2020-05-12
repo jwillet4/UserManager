0320d6bc5c702b4bdbaa777a870274154e285b01
jlwille3 <jlwille3@ncsu.edu>
2020-05-12 13:17:51 -0400

    Added group and user models; Added test data for user management

--- usermanager/src/app/user-management/user-management.component.ts
+++ usermanager/src/app/user-management/user-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { logWarnings } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-user-management',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManagementComponent implements OnInit {
 
-  constructor() { }
+  public users: Array<User>; 
+
+  constructor() {
+    this.users = [
+      {id: 1, first_name: "Admin", last_name: "Adminson", email: "admin@admin.com"},
+      {id: 2, first_name: "Logan", last_name: "Willett", email: "loganwillett96@gmail.com"},
+      {id: 3, first_name: "Test", last_name: "Test", email: "test@gmail.com"},
+    ];
+  }
 
   ngOnInit(): void {
   }
