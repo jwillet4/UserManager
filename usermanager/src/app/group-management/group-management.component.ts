07a05865b0c5597cf5eb29eb8af7ad68ede6ce86
jlwille3 <jlwille3@ncsu.edu>
2020-05-12 20:32:19 -0400

    Added user_group model; Added first steps of kendo editable grid

--- usermanager/src/app/group-management/group-management.component.ts
+++ usermanager/src/app/group-management/group-management.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UserGroup } from '../models/user-group';
+import { Group } from '../models/group';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AddEvent, EditEvent, GridComponent } from '@progress/kendo-angular-grid';
+import { groupBy, GroupDescriptor } from '@progress/kendo-data-query';
 
 @Component({
   selector: 'app-group-management',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupManagementComponent implements OnInit {
 
-  constructor() { }
+  public user_groups: Array<UserGroup>; 
+  public groups: Array<Group>; 
+
+  public formGroup: FormGroup;
+
+  @ViewChild(GridComponent)
+  private grid: GridComponent;
+  private editedRowIndex: number;
+
+  constructor() { 
+    this.user_groups = [
+      {id: 1, first_name: "Admin", last_name: "Adminson", group_name: "Goon"},
+      {id: 2, first_name: "Logan", last_name: "Willett", group_name: "Goon"},
+      {id: 3, first_name: "Test", last_name: "Test", group_name: "Jerk"},
+    ];
+    //Pull user data
+  }
 
   ngOnInit(): void {
+  }
+
+  public cellClickHandler({ isEdited, dataItem, rowIndex }): void {
+    if (isEdited || (this.formGroup && !this.formGroup.valid)) {
+        return;
+    }
+
+    //this.saveCurrent();
+
+    this.formGroup = new FormGroup({
+      'group_name': new FormControl(dataItem.group_name),
+    });
+    this.editedRowIndex = rowIndex;
+
+    this.grid.editRow(rowIndex, this.formGroup);
   }
 
 }
