7671786765eb80cd1bcd6494c44e1b78458692b4
Logan Willett <loganwillett96@gmail.com>
2020-05-15 16:24:11 -0400

    Finished creating controller and created base users call.

---
+++ usermanagerserver/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using webapi.Models;
+
+namespace usermanagerserver.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            var dc = new UserManagementContext();
+            return dc.User.ToList();
+        }
+
+        // [HttpGet("[action]")]
+        // public List<User> Test()
+        // {
+        //     var dc = new UserManagementContext();
+        //     return dc.User.ToList();
+        // }
+    }
+}
