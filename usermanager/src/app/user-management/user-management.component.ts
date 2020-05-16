1bba7e6ca60e6638ff400491f7c2251e3e3ee69b
Logan Willett <loganwillett96@gmail.com>
2020-05-16 19:38:01 -0400

    Added comments

--- usermanager/src/app/user-management/user-management.component.ts
+++ usermanager/src/app/user-management/user-management.component.ts
@@ -13,49 +13,56 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./user-management.component.scss']
 })
 export class UserManagementComponent implements OnInit {
-
+  //Observed from user service; used to populate the grid
   public users: Array<User>; 
 
+  //Tracks form group to add new user
   public formGroup: FormGroup;
 
+  //Tracks row index when editing
   private editedRowIndex: number;
 
   constructor(private titleService: Title, private userService: UserService) {
+    //Sets page title
     titleService.setTitle('User Manager');
   }
 
   ngOnInit(): void {
+    //Subscribes to observable in user service
     this.userService.users.subscribe(users => this.users = users);
   }
 
+  //Opens inline editor and creates corresponding form
   public addHandler({sender}) {
     this.closeEditor(sender);
-
     this.formGroup = new FormGroup({
       'first_name': new FormControl('', Validators.compose([Validators.required, Validators.max(45)])),
       'last_name': new FormControl('', Validators.compose([Validators.required, Validators.max(45)])),
       'email': new FormControl('', Validators.compose([Validators.required, Validators.max(45)])),
     });
-
     sender.addRow(this.formGroup);
   }
 
+  //Closes the in grid editor and resets vars
   private closeEditor(grid, rowIndex = this.editedRowIndex) {
     grid.closeRow(rowIndex);
     this.editedRowIndex = undefined;
     this.formGroup = undefined;
   }
 
+  //Calls user service to add new user
   public saveHandler({sender, rowIndex, formGroup, isNew}) {
     const user: User = formGroup.value;
     this.userService.addUser(user);
     sender.closeRow(rowIndex);
   }
 
+  //Closes inline editor
   public cancelHandler({sender, rowIndex}) {
     this.closeEditor(sender, rowIndex);
   }
 
+  //Calls user service to remove selected user
   public removeHandler({dataItem}) {
     this.userService.removeUser(dataItem);
   }
