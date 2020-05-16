1cde2f6e5c505e2ec2d5503386b9ed6534abac78
Logan Willett <loganwillett96@gmail.com>
2020-05-16 19:58:33 -0400

    Added comments

--- usermanagerserver/Controllers/UserController.cs
+++ usermanagerserver/Controllers/UserController.cs
@@ -14,6 +14,7 @@ namespace usermanagerserver.Controllers
     public class UserController : ControllerBase
     {
         private readonly ILogger<UserController> _logger;
+        //Database context
         private UserService _us;
 
         public UserController(ILogger<UserController> logger)
@@ -22,18 +23,38 @@ namespace usermanagerserver.Controllers
             _us = new UserService();
         }
 
+        ///<summary>
+        ///Searches the database for all user entries
+        ///</summary>
+        ///<returns>
+        ///A list of users
+        ///</returns>
         [HttpGet]
         public List<User> Get()
         {
             return _us.getUsers();
         }
 
+        ///<summary>
+        ///Adds a user to the database
+        ///</summary>
+        ///<param name="user">A User object</param>
+        ///<returns>
+        ///A list of users
+        ///</returns>
         [HttpPost("[action]")]
         public List<User> AddUser(User user) 
         {
             return _us.addUser(user);
         }
 
+        ///<summary>
+        ///Deletes a user from the database
+        ///</summary>
+        ///<param name="userId">Integer id of user</param>
+        ///<returns>
+        ///A list of Users
+        ///</returns>
         [HttpDelete("[action]")]
         public List<User> DeleteUser(int userId) 
         {
