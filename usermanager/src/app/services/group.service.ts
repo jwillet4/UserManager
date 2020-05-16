589b89915013226f1563d168375a28b83be026dd
Logan Willett <loganwillett96@gmail.com>
2020-05-16 19:44:25 -0400

    Added comments

--- usermanager/src/app/services/group.service.ts
+++ usermanager/src/app/services/group.service.ts
@@ -8,17 +8,20 @@ import { HttpClient, HttpHeaderResponse, HttpHeaders, HttpParams } from '@angula
   providedIn: 'root'
 })
 export class GroupService {
-
+  //Observable for groups list
   private groupSource: BehaviorSubject<Array<Group>>;
   public groups;
 
+  //Observable for usergroups list
   private userGroupSource: BehaviorSubject<Array<UserGroup>>;
   public userGroups;
 
+  //Info to send http requests to api
   private httpClient: HttpClient;
   private baseUrl: string;
 
   constructor(private http: HttpClient) { 
+    //Observable for user data
     this.groupSource = new BehaviorSubject<Array<Group>>([]);
     this.groups = this.groupSource.asObservable();
 
@@ -28,10 +31,12 @@ export class GroupService {
     this.httpClient = http;
     this.baseUrl = "https://localhost:5001/group"
 
+    //Pulls groups and usergroups data
     this.getGroups();
     this.getUserGroups();
   }
 
+  //Fetches list of existing groups
   public getGroups(): void {
     this.httpClient.get<Array<Group>>(this.baseUrl).subscribe(result => {
       this.groupSource.next(result);
@@ -40,6 +45,7 @@ export class GroupService {
     });
   }
 
+  //Fetches list of usergroups
   public getUserGroups(): void {
     this.httpClient.get<Array<UserGroup>>(this.baseUrl + "/UserGroups").subscribe(result => {
       this.userGroupSource.next(result);
@@ -48,6 +54,7 @@ export class GroupService {
     });
   }
 
+  //Adds a new group to assign to users
   public addGroup(group: string): void {
     const headers = new HttpHeaders().set('content-type', 'application/json');
     this.httpClient.post<Array<Group>>(this.baseUrl + '/AddGroup', new Group(group), { headers }).subscribe( result => {
@@ -57,6 +64,7 @@ export class GroupService {
     });
   }
 
+  //Deletes given group from database
   public removeGroup(group: Group): void {
     const httpParams = new HttpParams().set('groupId', group.id.toString());
     this.httpClient.delete<Array<Group>>(this.baseUrl + '/DeleteGroup', { params: httpParams }).subscribe( result => {
@@ -67,6 +75,7 @@ export class GroupService {
     });
   }
 
+  //Changes users group to new selected group
   public editUserGroup(userGroup: UserGroup, newGroup: Group): void {
     const headers = new HttpHeaders().set('content-type', 'application/json');
     this.httpClient.put<Array<UserGroup>>(this.baseUrl + '/ChangeGroup', new UserGroupChangeDTO(userGroup.uid, newGroup.id), { headers }).subscribe( result => {
