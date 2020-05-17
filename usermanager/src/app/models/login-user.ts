a7ad6f5b7d1ff84b4f4321944496d4d900814667
Logan Willett <loganwillett96@gmail.com>
2020-05-17 14:13:51 -0400

    Changed login-user to hold a token

--- usermanager/src/app/models/login-user.ts
+++ usermanager/src/app/models/login-user.ts
@@ -4,12 +4,14 @@ export class LoginUser {
     last_name: string;
     email: string;
     admin_status: boolean;
+    token
 
-    constructor(uid: number, first_name: string, last_name: string, email: string, admin_status: boolean) {
+    constructor(uid: number, first_name: string, last_name: string, email: string, admin_status: boolean, token: string) {
         this.uid = uid;
         this.first_name = first_name;
         this.last_name = last_name;
         this.email = email;
         this.admin_status = admin_status;
+        this.token = token;
     }
 }
