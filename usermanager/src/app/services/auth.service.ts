58b80fd23914690e89d6bf85b061b2681142094e
Logan Willett <loganwillett96@gmail.com>
2020-05-17 12:33:24 -0400

    Created login user object and set up auth call outline

--- usermanager/src/app/services/auth.service.ts
+++ usermanager/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@angular/core';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
+import { LoginUser } from '../models/login-user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,24 @@ import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 export class AuthService {
   STORAGE_KEY = "authCookie";
 
+  private authenticated: boolean;
+
+  private loginUser: LoginUser;
+
   constructor(@Inject(LOCAL_STORAGE) private storage) {
     console.log("Test:", this.storage.get(this.STORAGE_KEY));
     this.storage.set(this.STORAGE_KEY, "test");
   }
 
-  authenticateUser(): Boolean {
+  async authenticateUser(): Promise<LoginUser> {
+    if (this.storage.get(this.STORAGE_KEY)) {
+      //Send api call
+    }
+    else {
+      //Redirect
+    }
 
-
-    return false;
+    
+    return new Promise(null);
   }
 }
