811ebcf21f6bdf1bcb96e00eb2375744f25e5444
Logan Willett <loganwillett96@gmail.com>
2020-05-17 14:39:36 -0400

    Token authentication working

--- usermanager/src/app/services/auth.service.ts
+++ usermanager/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 import { LoginUser } from '../models/login-user';
+import { HttpClient, HttpHeaderResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,29 +13,39 @@ export class AuthService {
 
   private loginUser: LoginUser;
 
-  constructor(@Inject(LOCAL_STORAGE) private storage) {
+  //Info to send http requests to api
+  private httpClient: HttpClient;
+  private baseUrl: string;
+
+  constructor(@Inject(LOCAL_STORAGE) private storage, private http: HttpClient) {
     console.log("Test:", this.storage.get(this.STORAGE_KEY));
     this.storage.set(this.STORAGE_KEY, "test");
+
+    this.httpClient = http;
+    this.baseUrl = "https://localhost:5001/login"
+
+    this.authenticateUser();
   }
 
   async authenticateUser(): Promise<LoginUser> {
     if (this.storage.get(this.STORAGE_KEY)) {
       //Send api call
-
-      return new Promise((res, rej) => {
-        rej(null);
-      })
-
+      const httpParams = new HttpParams().set('token', this.storage.get(this.STORAGE_KEY));
+      const promise = this.httpClient.get<LoginUser>(this.baseUrl + '/LoginToken', { params: httpParams }).toPromise()
+      //Handle promise and return
+      promise.then((result) => {
+        return result;
+      }).catch((rej) => {
+        console.log(rej)
+      });
+      return promise;
     }
-    else {
-      return new Promise((res, rej) => {
-        rej(null);
-      })
-    }
+    return null;
   }
 
   async login(email: string): Promise<boolean> {
     //Send api call, return true if auth
+    
     return new Promise((res, rej) => {
       res(true);
     });
